0ef43fb58e53082f89d442368dc9bd7de1003640
biplavbarua <innocentsavage.bb@gmail.com>
2025-02-06 14:57:00 +0530

    2D Arrays

---
+++ Accessing_2D_Array.java
@@ -0,0 +1,12 @@
+public class Accessing_2D_Array {
+    public static void main(String[] args) {
+        //declare and initialize a 3x3 matrix
+        int matrix[][] =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println(matrix[1][0]);
+    }
+}
---
+++ For_Each_Loop_1D.java
@@ -0,0 +1,11 @@
+public class For_Each_Loop_1D {
+    public static void main (String[] args)
+    {
+        int[] array = {10, 20, 30, 40, 50};
+        System.out.println("The elements of the array: ");
+        for(int i: array)
+        {
+            System.out.println(i+" ");
+        }
+    }
+}
