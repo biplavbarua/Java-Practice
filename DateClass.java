8c9c2f1ee0285ac9e4682a3e4e3926bb2821b157
biplavbarua <innocentsavage.bb@gmail.com>
2025-03-26 10:42:27 +0530

    DateTimeFormatter

--- DateClass.java
+++ DateClass.java
@@ -1,11 +1,13 @@
-import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 class DateClass {
     public static void main(String[] args) {
-        LocalDateTime abc = LocalDateTime.now();
-        System.out.println(abc);
-        ZonedDateTime zdt = ZonedDateTime.now();
-        System.out.println(zdt);
+        LocalDate date = LocalDate.now();
+        System.out.println("Current Date: " + date);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = date.format(formatter);
+        System.out.println("Formatted Date: " + formattedDate);
     }
 }
